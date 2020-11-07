ecc1443c4672efab6c71014f32e833cd2d16fcba
DJ Hersh <DanielJHersh@gmail.com>
2020-11-06 22:04:46 -0800

    Got all the codes on the page I'll need to make all my API calls for the info I'll need

--- assets/script.js
+++ assets/script.js
@@ -2,26 +2,45 @@ var apiKey1 = "d3d6560ad567216dabcc0351c22d0fad";
 var apiKey2 = "41490cc9c30ac5171880367eb41b32e3";
 var apiKey3 = "c8365256edd594643ad843e3f1e0c7ad";
 
-city = "los angeles";
+city = "seattle";
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey3;
-
-var lat = "";
-var lon = "";
+var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?units=imperial&q=" + city + "&appid=" + apiKey1;
 
 $.ajax({
     url: queryURL,
     method: "GET"
 }).then(function(response) {
     console.log(response);
-    lat = response.coord.lat;
-    lon = response.coord.lon;
-    console.log(lat);
+    console.log(response.name);
+    console.log(response.main.temp);
+    console.log(response.main.humidity);
+    console.log(response.wind.speed);
+    var lat = response.coord.lat;
+    var lon = response.coord.lon;
     var uvURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey2;
     $.ajax({
         url: uvURL,
         method: "GET"
     }).then(function(response) {
         console.log(response);
+        console.log(response.value);
+    });
+    var oneCallURL = "http://api.openweathermap.org/data/2.5/onecall?units=imperial&lat=" + lat + "&lon=" + lon + "&appid=" + apiKey2;
+    $.ajax({
+        url: oneCallURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
     })
 });
+
+$.ajax({
+    url: forecastURL,
+    method: "GET"
+}).then(function(response) {
+    console.log(response);
+});
+
+// instructions on website how to set icons
+var iconURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
 
