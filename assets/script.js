93de13b14dd65999b6f151943f70ae7624eddac1
DJ Hersh <DanielJHersh@gmail.com>
2020-11-07 23:28:49 -0800

    Got rid of redundant variables

--- assets/script.js
+++ assets/script.js
@@ -28,17 +28,11 @@ function getWeather(city) {
         method: "GET"
     }).then(function(response) {
         var name = response.name;
-        var date = convertDate(response.dt);
-        $("#city-name").text(name + " (" + date + ")");
-        var temp = response.main.temp;
-        $("#current-temp").text(temp + " °F");
-        var humidity = response.main.humidity;
-        $("#current-humidity").text(humidity + "%");
-        var wind = response.wind.speed;
-        $("#current-wind").text(wind + " MPH");
-        var lat = response.coord.lat;
-        var lon = response.coord.lon;
-        var oneCallURL = "https://api.openweathermap.org/data/2.5/onecall?units=imperial&lat=" + lat + "&lon=" + lon + "&appid=" + apiKey2;
+        $("#city-name").text(name + " (" + convertDate(response.dt) + ")");
+        $("#current-temp").text(response.main.temp + " °F");
+        $("#current-humidity").text(response.main.humidity + "%");
+        $("#current-wind").text(response.wind.speed + " MPH");
+        var oneCallURL = "https://api.openweathermap.org/data/2.5/onecall?units=imperial&lat=" + response.coord.lat + "&lon=" + response.coord.lon + "&appid=" + apiKey2;
         $.ajax({
             url: oneCallURL,
             method: "GET"
@@ -62,19 +56,16 @@ function getWeather(city) {
             }
             $("#forecast-cards").empty();
             for (var i = 1; i < 6; i++) {
-                var forecastIconURL = "http://openweathermap.org/img/wn/" + response.daily[i].weather[0].icon + "@2x.png";
-                var forecastTemp = response.daily[i].temp.day;
-                var forecastHumidity = response.daily[i].humidity;
                 var col = $("<div class='col'>");
                 var card = $("<div class='card'>");
                 var body = $("<div class='card-body bg-primary'>");
                 var cardDate = $("<h3 class='card-title text-white'>");
                 cardDate.text(convertDate(response.daily[i].dt));
-                var cardIcon = $("<img src=" + forecastIconURL + " alt='weather icon'>");
+                var cardIcon = $("<img src='http://openweathermap.org/img/wn/" + response.daily[i].weather[0].icon + "@2x.png' alt='weather icon'>");
                 var cardTemp = $("<p class='card-text text-white'>");
-                cardTemp.text("Temp: " + forecastTemp + " °F");
+                cardTemp.text("Temp: " + response.daily[i].temp.day + " °F");
                 var cardHumidity = $("<p class='card-text text-white'>");
-                cardHumidity.text("Humidity: " + forecastHumidity + "%");
+                cardHumidity.text("Humidity: " + response.daily[i].humidity + "%");
                 body.append(cardDate, cardIcon, cardTemp, cardHumidity);
                 card.append(body);
                 col.append(card);
@@ -97,8 +88,6 @@ $(".btn").on("click", function(event) {
     event.preventDefault();
     if ($("#city-input").val()) {
         var city = $("#city-input").val().split(" ").join("+");
-        console.log(city);
-        $("#uv").removeClass();
         $("#city-input").val("");
         getWeather(city);
     }
